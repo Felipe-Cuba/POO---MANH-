47b846cef2bfefcb6fb705d3a42fc7b1aee44ccc
Felipe-Cuba <felipeccuba1803@gmail.com>
2022-08-19 08:02:26 -0300

    Primeiros exercicios

---
+++ Estrutura_Decicao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+double salario;
+
+Console.Write("Digite o salário:");
+salario = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Seu salário é R$ {salario}");
+
+if (salario > 5000)
+    Console.WriteLine("Paga imposto INSS");
+
+else if (salario <= 1000)
+    Console.WriteLine("Não paga impostos");
+
+else
+    Console.WriteLine("Paga IR");
+
+int mes = 1;
+
+switch(mes)
+{
+    case 1: Console.WriteLine("Janeiro");
+        break;
+    case 2: Console.WriteLine("Fevereiro");
+        break;
+    default: Console.WriteLine("Mês Inválido");
+        break;
+}
+
+
+
---
+++ Estrutura_Repeticao/Program.cs
@@ -0,0 +1,3 @@
+﻿for (int x = 0; x < 5; x++)
+    Console.WriteLine(x);
+    
