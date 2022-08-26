bd07782933e3c20acfad2a91049e7100c856a6d3
Felipe-Cuba <felipeccuba1803@gmail.com>
2022-08-26 11:08:10 -0300

    Aula 3 Final: Criando um array com objeto Conta

---
+++ ArrayConta/Conta.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayConta
+{
+    public class Conta
+    {
+        //Delcaração de atributos
+
+        public int numero;
+        public string titular;
+        public double saldo;
+
+       //Delcaração dos metódos 
+        
+        public void Sacar(double valorSaque){
+
+            saldo -= valorSaque;
+
+        }
+
+        public void Depositar(double valorDeposito){
+            saldo += valorDeposito;
+        }
+
+        public void Transferir(double valorTransferir, Conta objConta){
+            //Passagem de parametro por valor usa valorTrasnferir
+            //Passagem de valor por refêrencia usa objConta
+            saldo -= valorTransferir; //Objeto atual
+            objConta.saldo += valorTransferir; //Objeto a tranferir
+        }
+
+        public void MostrarAtributos(){
+            Console.WriteLine($"------------------------- Conta de {titular}:{numero} -------------------------------");
+            Console.WriteLine($"\nNome do titular: {titular}\tNumero da conta: {numero}\tSaldo da conta: {saldo:c}\n");
+        }
+    }
+}
---
+++ ArrayConta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ArrayConta;
+
+
+// Declaração do vetor de contas
+Conta[] vetorConta = new Conta[3];
+
+
+for(int i = 0;i < vetorConta.Length; i++){
+    // Instancia cada índice
+    vetorConta[i] = new Conta();
+
+    Console.WriteLine("-=-=-=-=-=-=-=-= Cadastro de conta -=-=-=-=-=-=-=-=-");
+    //Cadastro do número da conta
+    Console.Write("\nCadastre o número da conta: ");
+    vetorConta[i].numero = Convert.ToInt32(Console.ReadLine());
+    //Cadastro do titular da conta
+    Console.Write("\nCadastre o titular da conta: ");
+    vetorConta[i].titular = Console.ReadLine();
+    //Cadastro do saldo da conta
+    Console.Write("\nCadastre o saldo da conta: ");
+    vetorConta[i].saldo = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+}
+
+foreach (Conta c in vetorConta){
+    c.MostrarAtributos();
+}
