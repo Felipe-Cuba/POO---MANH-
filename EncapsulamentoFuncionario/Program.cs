fdeb71bafa43d7d2c75e90015b371d82c25a97d2
Felipe Cuba <felipeccuba1803@gmail.com>
2022-09-02 09:21:27 -0300

    Encapsulamento Inicio

---
+++ EncapsulamentoFuncionario/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EncapsulamentoFuncionario;
+
+Funcionario f1 = new Funcionario();
+
+f1.Codigo = 1;
+f1.Nome = "Ana";
+f1.Salario = 1000;
+
+Console.WriteLine($"Nome do funcionário: {f1.Nome}");// get
+
+if(f1.Salario > 5000){
+    Console.WriteLine("Paga imposto");
+} else {
+    Console.WriteLine("Não paga imposto");
+}
